9bc277bbec073138f102adf3617e57914efac957
nit567esh <nit567esh@gamil.com>
2021-05-23 19:12:28 +0530

    first commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mailreport',
-    version='1.0.0',
+    version='1.0.1',
     author="Nitesh Kumar",
     author_email="nit567esh@gmail.com",
     description="Python Pkg for sending autmated reports on email",
